ba13874b35779739b54f385ba44b48855344dea4
OmarCodes2 <omarcodes2@gmail.com>
2022-11-19 15:01:46 -0500

    fixed bad oauth error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {useEffect, useState} from "react";
+
 import axios from 'axios';
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     const RESPONSE_TYPE = "token"
 
     const [token, setToken] = useState("")
-
+    const [searchKey, setSearchKey] = useState("")
+    const [artists, setArtists] = useState([])
 
     // const getToken = () => {
     //     let urlParams = new URLSearchParams(window.location.hash.replace("#","?"));
@@ -38,23 +40,49 @@ function App() {
         window.localStorage.removeItem("token")
     }
 
+    const searchArtists = async (e) => {
+        e.preventDefault()
+        const {data} = await axios.get("https://api.spotify.com/v1/me/top/artists", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            /*params: {
+                q: searchKey,
+                type: "artist"
+            }*/
+        })
+
+        setArtists(data.items)
+    }
+
+    const renderArtists = () => {
+        return artists.map(artist => (
+            <div key={artist.id}>
+                {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
+                {artist.name}
+            </div>
+        ))
+    }
 
     return (
         <div className="App">
             <header className="App-header">
                 <h1>Spotify React</h1>
                 {!token ?
-                    <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
+                    <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=user-top-read`}>Login
                         to Spotify</a>
                     : <button onClick={logout}>Logout</button>}
 
                 {token ?
-                    <div>
-                      You are in
-                    </div>
+                    <form onSubmit={searchArtists}>
+                        <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+                        <button type={"submit"}>Search</button>
+                    </form>
 
                     : <h2>Please login</h2>
                 }
+
+                {renderArtists()}
 
             </header>
         </div>
